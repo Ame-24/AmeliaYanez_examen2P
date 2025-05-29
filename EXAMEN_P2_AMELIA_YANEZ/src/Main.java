5e902893263dbb14cd841d737cc7e33f584f8ac4
Ame-24 <salome24ys2005@gmail.com>
2025-05-29 17:26:36 -0500

    Commit 2 FINAL

--- EXAMEN_P2_AMELIA_YANEZ/src/Main.java
+++ EXAMEN_P2_AMELIA_YANEZ/src/Main.java
@@ -10,6 +10,50 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Ingrese saldo inicial: ");
+        float saldoInicial = sc.nextFloat();
+
+        System.out.print("Ingrese tasa de interés anual (%): ");
+        float tasaAnual = sc.nextFloat();
+
+        AY_CUENTAAHORROS cuenta = new AY_CUENTAAHORROS(saldoInicial, tasaAnual);
+
+        int opcion;
+                do{
+                    System.out.println("\nMenú");
+                    System.out.println(" 1. Depositar");
+                    System.out.println(" 2. Retirar");
+                    System.out.println(" 3. Ver saldo");
+                    System.out.println(" 4. Salir");
+                    System.out.println("Número total de transacciones: " + (cuenta.numeroDepositos + cuenta.numeroRetiros));
+                    opcion= sc.nextInt();
+                    sc.nextLine();
+
+                    switch (opcion) {
+                        case 1:
+                            System.out.print("Cantidad a depositar: ");
+                            double deposito = sc.nextDouble();
+                            cuenta.depositar(deposito);
+                            break;
+                        case 2:
+                            System.out.print("Cantidad a retirar: ");
+                            double retiro = sc.nextDouble();
+                            cuenta.retirar(retiro);
+                            break;
+                        case 3:
+                            cuenta.imprimir();
+                            break;
+                        case 4:
+                            System.out.println("Gracias profe.");
+                            break;
+                        default:
+                            System.out.println("Opción no válida.");
+
+                    }
+                } while (opcion != 4);
+
+        sc.close();
     }
 }
